83e4ddfb7e396e7703f0909a674ea38f3638645b
Ben | Ru1ned <145772142+BenHrzr@users.noreply.github.com>
2024-12-13 09:07:36 +0100

    Create Bullet.java

---
+++ java-Strike2D (13.12)/Bullet.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
+
+public class bullet extends Actor
+{
+    public void act() 
+    {
+        fly();        
+    } 
+    public void fly()
+    {
+        move(15);
+    }
+    }
+
